335cb5c335c8facc31be0d926e1a4c7c0a156a98
Marco Garzon <77040582+The-Spaghetti-Is-Ready@users.noreply.github.com>
2024-09-21 23:48:31 +0800

    Fixed fgets so it's now fetching the correct amount of bytes from stream.

--- sourcecode/Part1/shellfunctions.c
+++ sourcecode/Part1/shellfunctions.c
@@ -3,7 +3,7 @@
 char * GetKBInput() { //get input from keyboard
     char * input = (char *) malloc(MAX_STR_SIZE * sizeof(char)); //128 character-limit
 
-    fgets(input, 128, stdin);
+    fgets(input, MAX_STR_SIZE, stdin);
     
     input[strcspn(input, "\n")] = '\0'; //remove newline
 
